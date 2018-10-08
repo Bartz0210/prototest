568f2002d648801101e6f93f393b0eb678ba2daa
tim <tim@iMacGeluchunke2.fritz.box>
2018-10-08 16:23:51 +0200

    Shader finalized with lighting

--- src/renderer/materials/shaders/cursor.fragment.glsl
+++ src/renderer/materials/shaders/cursor.fragment.glsl
@@ -2,6 +2,7 @@ precision highp float;
 precision highp int;
 precision lowp sampler2D;
 precision lowp samplerCube;
+// precision highp vec3;
 
 uniform mediump float uTime; 
 uniform vec3 uColor;
@@ -10,16 +11,21 @@ uniform float uFrequency;
 varying float vLighting;
 varying float vRefraction;
 
-void main(void) {
 
+void main(void) {
+  mediump vec3 color;
   mediump float frq = uFrequency/6.0;
   mediump float osc = (sin(uTime/1000.0*frq));
   mediump float green = osc;
+  
+
 
   if(green<0.5){
     green = 0.5;
   }
+  color = vec3(osc, green, 1.0);
 
-  gl_FragColor = vec4(osc, green, 1, vRefraction);
+  //gl_FragColor = vec4(osc, green, 1, vRefraction);
+  gl_FragColor = vec4(color * vLighting, vRefraction);
   //gl_FragColor = vec4(uColor * vLighting, vRefraction);
 }
